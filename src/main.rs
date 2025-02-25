246c2d96552a3a8e45c71956baba6a9b5c88bf19
daalfox <daalfox@gmail.com>
2025-02-25 12:00:04 +0330

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,16 @@ struct Message {
     body: Body,
 }
 
+impl Message {
+    fn into_reply(self, msg_id: usize) -> Option<Self> {
+        Some(Self {
+            src: self.dest,
+            dest: self.src,
+            body: self.body.into_reply(msg_id)?,
+        })
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 struct Body {
     #[serde(rename = "msg_id")]
@@ -19,6 +29,15 @@ struct Body {
     in_reply_to: Option<usize>,
     #[serde(flatten)]
     payload: Payload,
+}
+impl Body {
+    fn into_reply(self, msg_id: usize) -> Option<Self> {
+        Some(Self {
+            id: Some(msg_id),
+            in_reply_to: self.id,
+            payload: self.payload.into_reply()?,
+        })
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -37,6 +56,16 @@ enum Payload {
         echo: String,
     },
 }
+impl Payload {
+    fn into_reply(self) -> Option<Self> {
+        match self {
+            Payload::Init { .. } => Some(Payload::InitOk),
+            Payload::Echo { echo } => Some(Payload::EchoOk { echo }),
+            Payload::InitOk => None,
+            Payload::EchoOk { .. } => None,
+        }
+    }
+}
 
 struct Node {
     id: String,
@@ -51,49 +80,23 @@ impl Node {
         }
     }
 
+    /// increments node's `last_sent_msg_id` field and returns it
+    fn increment_id(&mut self) -> usize {
+        self.last_sent_msg_id += 1;
+        self.last_sent_msg_id
+    }
+
     fn handle(&mut self, input: Message, stdout: &mut StdoutLock) -> anyhow::Result<()> {
-        let id = self.last_sent_msg_id + 1;
+        let reply = input.into_reply(0);
 
-        let mut reply = None;
-        match input.body.payload {
-            Payload::Init { node_id, .. } => {
-                self.id = node_id;
-
-                reply = Message {
-                    src: input.dest,
-                    dest: input.src,
-                    body: Body {
-                        id: Some(id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::InitOk,
-                    },
-                }
-                .into();
-            }
-            Payload::Echo { echo } => {
-                reply = Message {
-                    src: input.dest,
-                    dest: input.src,
-                    body: Body {
-                        id: Some(id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::EchoOk { echo },
-                    },
-                }
-                .into();
-            }
-            Payload::InitOk => {}
-            Payload::EchoOk { .. } => {}
-        };
-
-        if let Some(reply) = reply {
+        if let Some(mut reply) = reply {
+            reply.body.id = Some(self.increment_id());
             serde_json::to_writer(&mut *stdout, &reply).context("couldn't serialize response")?;
             stdout
                 .write_all(b"\n")
                 .context("failed to write trailing new line")?;
         };
 
-        self.last_sent_msg_id = id;
         Ok(())
     }
 }
